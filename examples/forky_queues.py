fix some numerical issues

examples/forky_queues.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from handlers.output_handler import OutputHandler
 
 # Now you can import using the project structure
-from src.utils.visualizer import NetworkVisualizer
+from src.utils.visualizer import NetworkVisualizer, progress_callback
 from src.LTM.network import Network
 
 if __name__ == "__main__":
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         'k_critical': 2,
         'k_jam': 10,
         'unit_time': 10,
-        'peak_lambda': 20,
+        'peak_lambda': 25,
         'base_lambda': 5,
         'simulation_steps': 800,
     }
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         network_env.network_loading(t)
         if t == 100:
             network_env.update_turning_fractions_per_node(node_ids=[1],
-                                                          new_turning_fractions=np.array([[1, 0, 0.5, 0.5, 0, 1]]))
+                                                          new_turning_fractions=np.array([[1, 0, 0.5, 0.5, 0, 1]])) #[1_2, 1_4, 1_0, 1_4, 1_0, 1_2]
 
     # Construct paths relative to the project root
     output_dir = os.path.join("..", "outputs")
@@ -61,5 +61,13 @@ if __name__ == "__main__":
     import matplotlib
     matplotlib.use('macosx')
     visualizer = NetworkVisualizer(simulation_dir=os.path.join(output_dir, "forky_queues"))
-    ani = visualizer.animate_network(start_time=0, end_time=params["simulation_steps"], interval=100, edge_property='density')
+    anim = visualizer.animate_network(start_time=0, end_time=params["simulation_steps"], interval=100, edge_property='density')
+    # MP4
+    writer = matplotlib.animation.FFMpegWriter(fps=15, metadata=dict(artist='Me'),
+                                             bitrate=2000)
+
+    # Save the animation as MP4
+    anim.save(os.path.join(output_dir, "forky_queues", "forky_queues_cong.mp4"),
+              writer=writer,
+              progress_callback=progress_callback)
     plt.show()

src/LTM/link.py
@@ -130,7 +130,7 @@ class Link(BaseLink):
             self.sending_flow = 0
             return self.sending_flow
         elif time_step - tau < 0:           # for the highly congested stage
-            self.sending_flow = self.link_flow[time_step - 1] * self.unit_time
+            self.sending_flow = max(1, np.floor(self.link_flow[time_step - 1] * self.unit_time)) # ensure when congested, the number of peds going through is at least 1
             return self.sending_flow
         else:                  # for the normal stage and the congestion stage
             sending_flow_boundary = self.cumulative_inflow[time_step - tau] - self.cumulative_outflow[time_step - 1]
@@ -143,7 +143,7 @@ class Link(BaseLink):
                 # with a certain probability, the diffusion flow will be the sending flow
                 # if np.random.rand() < 0.5:
                 #     self.sending_flow = diffusion_flow
-                self.sending_flow = self.link_flow[time_step - 1] * self.unit_time
+                self.sending_flow = max(1, np.floor(self.link_flow[time_step - 1] * self.unit_time))
         return max(0, self.sending_flow)
 
     def cal_receiving_flow(self, time_step: int) -> float:

src/LTM/network.py
@@ -52,7 +52,14 @@ class Network:
         np.random.seed(seed)
         # after a certian time, the demand is decreasing
         demand = np.random.poisson(lam=lambda_t)
-        demand[time > 500] = 0
+        # create a spike at from t == 400 to t == 410
+        # if t == 400:
+        # demand[0:10] = 100
+        # demand[350:400] = 100
+        # demand[time > 10] = 0
+        # demand[time > 300] = 0
+        demand = np.ones(simulation_steps) * 1
+        demand[time > 300] = 0
         return demand
 
     def init_nodes_and_links(self, od_nodes: list, origin_nodes: list):

src/LTM/node.py
@@ -11,7 +11,7 @@ class Node:
         self.turning_fractions = None # 1D array, the length is the number of edges
         self.mask = None
         self.q = None
-        self.w = 1e-2
+        self.w = 1e-2 # penalty term for turning fractions
         self.source_num = None
         self.dest_num = None
         self.edge_num = None
@@ -168,9 +168,9 @@ class Node:
                 s[i] = self.demand[time_step]
             else:
                 s[i] = l.cal_sending_flow(time_step)
-                if l.link_id == "2_3":
-                    print(f'{time_step}:link {l.link_id} num peds: {l.link_flow[time_step-1]}')
-                    print(f'{time_step}:link {l.link_id} num peds: {l.sending_flow}')
+                # if l.link_id == "2_3":
+                #     print(f'{time_step}:link {l.link_id} num peds: {l.link_flow[time_step-1]}')
+                #     print(f'{time_step}:link {l.link_id} num peds: {l.sending_flow}')
         
         # Calculate receiving flows
         for j, l in enumerate(self.outgoing_links):
@@ -179,6 +179,8 @@ class Node:
             else:
                 #reverse link sending flow
                 r[j] = max(0, l.cal_receiving_flow(time_step) - l.reverse_link.num_pedestrians[time_step]) # consider the flow from the reverse link
+                # if l.link_id == "2_3": # simulate bottleneck
+                #     r[j] = 5
         
         self.solve(s, r)
         self.update_links(time_step)
@@ -197,11 +199,9 @@ class OneToOneNode(Node):
         # Ensure non-negative flows by using maximum of 0 and the minimum flow
         self.q = np.array([np.min([s[0],r[1]]), np.min([s[1],r[0]]),
                             np.min([s[1],r[0]]), np.min([s[0],r[1]])])
-        self.q = np.where(self.q < 0, 0, self.q)
-        # self.q = np.array([max(0, np.min([s[0],r[1]])), 
-        #             max(0, np.min([s[1],r[0]])),
-        #             max(0, np.min([s[1],r[0]]),
-        #             max(0, np.min([s[0],r[1]])))])
+        # self.q = np.where(self.q < 0, 0, self.q)
+        # min_val = 1 # ensure when congested, the number of peds going through is at least 1
+        # self.q = np.where(self.q > 0, np.maximum(min_val, np.floor(self.q)), 0)
         return
 
 class RegularNode(Node):
@@ -224,7 +224,7 @@ class RegularNode(Node):
             # res = linprog(c, A_ub=self.A_ub, b_ub=b_ub, bounds=bounds)
             res = linprog(c, A_ub=self.A_ub, b_ub=b_ub)
         if res.success:
-            flows = self.A_ub @ res.x
+            flows = self.A_ub @ np.floor(res.x)
             # Ensure non-negative flows and round down to nearest integer
-            self.q = np.maximum(0, np.floor(flows))
+            self.q = np.maximum(0, flows)
         return
c0b6ebdc6c6452a2d850394fa45e63c3785f1ab3
Manuel Garcia <gilbert.mg22@gmail.com>
2026-03-18 16:34:39 +0100

    fix: correct import path for NetworkEnvGenerator in test_delft_exp.py

--- tests/test_delft_exp.py
+++ tests/test_delft_exp.py
@@ -9,7 +9,7 @@ import sys
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
 
-from pednstream.utils.env_loader import NetworkEnvGenerator
+from pednstream.utils.network_env_generator import NetworkEnvGenerator
 from pednstream.utils.output_handler import OutputHandler
 from pednstream.utils.visualizer import NetworkVisualizer
 import matplotlib.pyplot as plt

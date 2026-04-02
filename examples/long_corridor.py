744b94ab4870da487a16970ccd5d41cebfab03d1
WaimenMak <weimingmai@uic.edu.cn>
2026-04-02 10:48:13 +0200

    update settings

--- examples/long_corridor.py
+++ examples/long_corridor.py
@@ -32,93 +32,93 @@ if __name__ == "__main__":
 
     # Initialize and run simulation
     # ''' Scenario 1 '''
-    # params = {
-    #     'unit_time': 10,
-    #     'simulation_steps': 600,
-    #     'default_link': {
-    #         'length': 100,  # make it to 50 to see spillback
-    #         'width': 2,
-    #         'free_flow_speed': 1.1,
-    #         'k_critical': 2,
-    #         'k_jam': 6,
-    #         'fd_type': 'yperman',  # type of fundamental diagram
-    #         'bi_factor': 1,  # factor for bi-directional FD
-    #         'controller_type': 'gate',  # type of controller
-    #     },
-    #     # 'controllers': {
-    #     #     'enabled': True,
-    #     #     'links': ["0-1","1-2","2-3","3-4","4-5"],
-    #     # },
-    #     'demand': {
-    #         "origin_0": {
-    #             "peak_lambda": 25,
-    #             "base_lambda": 5,
-    #         },
-    #         "origin_5": {
-    #             "peak_lambda": 25,
-    #             "base_lambda": 5,
-    #         }
-    #     }
+    params = {
+        'unit_time': 10,
+        'simulation_steps': 600,
+        'default_link': {
+            'length': 100,  # make it to 50 to see spillback
+            'width': 2,
+            'free_flow_speed': 1.1,
+            'k_critical': 2,
+            'k_jam': 6,
+            'fd_type': 'yperman',  # type of fundamental diagram
+            'bi_factor': 1,  # factor for bi-directional FD
+            'controller_type': 'gate',  # type of controller
+        },
+        # 'controllers': {
+        #     'enabled': True,
+        #     'links': ["0-1","1-2","2-3","3-4","4-5"],
+        # },
+        'demand': {
+            "origin_0": {
+                "peak_lambda": 25,
+                "base_lambda": 5,
+            },
+            "origin_5": {
+                "peak_lambda": 25,
+                "base_lambda": 5,
+            }
+        }
 
-    # }
-    # network_env = Network(adj, params, origin_nodes=[5, 0])
+    }
+    network_env = Network(adj, params, origin_nodes=[5, 0])
     # # # Set demand for nodes to zero after a certain time step
     # network_env.nodes[0].demand[300:] = np.zeros(600 - 300)
     # network_env.nodes[5].demand[300:] = np.zeros(600 - 300)
     # ''' Scenario 2 '''
-    params = {
-        'unit_time': 10,
-        'simulation_steps': 800,
-        'assign_flows_type': 'classic',
-        'default_link': {
-            'length': 50,  # make it to 50 to see spillback
-            'width': 2,
-            'free_flow_speed': 1.1,
-            'k_critical': 1,
-            'k_jam': 6,
-            'activity_probability': 0,  # probability of activity on the link
-            'fd_type': 'yperman',  # type of fundamental diagram
-            'bi_factor': 1,  # factor for bi-directional FD
-            'speed_noise_std': 0,  # whether to add noise to the speed
-            'controller_type': 'gate',  # type of controller
-        },
-        'links': {
-            '2_3': {
-                'length': 50,
-                'width': 1,
-                'free_flow_speed': 1.1,
-                'k_critical': 2,
-                'k_jam': 6,
-                'activity_probability': 0,  # probability of activity on the link
-                'fd_type': 'yperman',  # type of fundamental diagram
-                'bi_factor': 1,  # factor for bi-directional FD
-                'speed_noise_std': 0,  # whether to add noise to the speed
-                'controller_type': 'gate',  # type of controller
-            },
-        },
-        'demand': {
-            "origin_3": {
-                "peak_lambda": 50,
-                "base_lambda": 8,
-            },
-            "origin_2": {
-                "peak_lambda": 50,
-                "base_lambda": 8,
-            }
-        }
-    
-    }
-    network_env = Network(adj, params, origin_nodes=[2, 3])
-    network_env.update_turning_fractions_per_node(node_ids=[2, 3],
-                                                          new_turning_fractions=np.array([[0, 1, 0.5, 0.5, 0, 1],
-                                                                                          [1, 0, 0, 1, 0.5, 0.5]]))
+    # params = {
+    #     'unit_time': 10,
+    #     'simulation_steps': 800,
+    #     'assign_flows_type': 'classic',
+    #     'default_link': {
+    #         'length': 50,  # make it to 50 to see spillback
+    #         'width': 2,
+    #         'free_flow_speed': 1.1,
+    #         'k_critical': 1,
+    #         'k_jam': 6,
+    #         'activity_probability': 0,  # probability of activity on the link
+    #         'fd_type': 'yperman',  # type of fundamental diagram
+    #         'bi_factor': 1,  # factor for bi-directional FD
+    #         'speed_noise_std': 0,  # whether to add noise to the speed
+    #         'controller_type': 'gate',  # type of controller
+    #     },
+    #     'links': {
+    #         '2_3': {
+    #             'length': 50,
+    #             'width': 1,
+    #             'free_flow_speed': 1.1,
+    #             'k_critical': 2,
+    #             'k_jam': 6,
+    #             'activity_probability': 0,  # probability of activity on the link
+    #             'fd_type': 'yperman',  # type of fundamental diagram
+    #             'bi_factor': 1,  # factor for bi-directional FD
+    #             'speed_noise_std': 0,  # whether to add noise to the speed
+    #             'controller_type': 'gate',  # type of controller
+    #         },
+    #     },
+    #     'demand': {
+    #         "origin_3": {
+    #             "peak_lambda": 50,
+    #             "base_lambda": 8,
+    #         },
+    #         "origin_2": {
+    #             "peak_lambda": 50,
+    #             "base_lambda": 8,
+    #         }
+    #     }
+    #
+    # }
+    # network_env = Network(adj, params, origin_nodes=[2, 3])
+    # network_env.update_turning_fractions_per_node(node_ids=[2, 3],
+    #                                                       new_turning_fractions=np.array([[0, 1, 0.5, 0.5, 0, 1],
+    #                                                                                       [1, 0, 0, 1, 0.5, 0.5]]))
     # Set demand for nodes to zero after a certain time step
-    network_env.nodes[2].demand[0:30] = np.zeros(30)
-    network_env.nodes[3].demand[0:40] = np.zeros(40)
+    # network_env.nodes[2].demand[0:30] = np.zeros(30)
+    # network_env.nodes[3].demand[0:40] = np.zeros(40)
     # network_env.nodes[2].demand[80:100] = np.zeros(20)
     # network_env.nodes[3].demand[30:] = np.zeros(600 - 30)
 
-    network_env.visualize()
+    # network_env.visualize()
 
 
     # Run simulation

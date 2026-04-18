56770ac22872361259d1c9d1ad04f58c3a2a1e09
WaimenMak <weimingmai@uic.edu.cn>
2026-04-18 13:09:17 +0200

    update paths

--- examples/six_node.py
+++ examples/six_node.py
@@ -19,7 +19,7 @@ def main():
     # config_path = os.path.join(project_root, 'sim_params.yaml')
     # config = load_config(config_path)
     env_generator = NetworkEnvGenerator()
-    network_env = env_generator.create_network('od_flow_example')
+    network_env = env_generator.create_network('../data/od_flow_example')
     
     # Initialize network and run simulation
     # network_env = Network(**config)

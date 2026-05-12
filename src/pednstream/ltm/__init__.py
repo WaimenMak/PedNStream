fcc13155a2aebde5fa6e49816651898a781200e1
WaimenMak <weimingmai@uic.edu.cn>
2026-05-12 17:10:55 +0200

    refactor: update __init__.py to remove unused node types and include NodeFlowSolver

--- src/pednstream/ltm/__init__.py
+++ src/pednstream/ltm/__init__.py
@@ -5,18 +5,18 @@
 # @Software: PyCharm
 
 from .network import Network
-from .node import Node, OneToOneNode, RegularNode
+from .node import Node
 from .link import Link, Separator
 from .od_manager import ODManager
 from .path_finder import PathFinder
+from .solver import NodeFlowSolver
 
 __all__ = [
     'Network',
     'Node',
-    'OneToOneNode',
-    'RegularNode',
     'Link',
     'Separator',
     'ODManager',
-    'PathFinder'
+    'PathFinder',
+    'NodeFlowSolver',
 ]
